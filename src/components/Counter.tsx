4b31e08274170b80616c998c8b8d470733fb99cc
Taehee Kim <rabolution@gmail.com>
2024-10-27 21:36:26 +0900

    feat: 스토리북 셋업

---
+++ src/components/Counter.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0)
+    return (
+        <button className='btn btn-primary' onClick={() => {
+            setCount(n => n + 1)
+        }}>
+            {count}
+        </button>
+    )
+}
+
+export default Counter;
---
+++ src/stories/Counter.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+ 
+import Counter from '../components/Counter';
+ 
+const meta: Meta<typeof Counter> = {
+  component: Counter,
+};
+ 
+export default meta;
+type Story = StoryObj<typeof Counter>;
+ 
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Counter',
+  },
+};
