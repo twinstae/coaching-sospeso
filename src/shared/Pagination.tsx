e7d6f64f743a7815c9fca279f2948f9a7ee1dc37
Taehee Kim <rabolution@gmail.com>
2024-11-09 12:07:34 +0900

    feat: prop으로 현재 페이지를 받는다

--- src/shared/Pagination.tsx
+++ src/shared/Pagination.tsx
@@ -1,13 +1,13 @@
 import { Link } from '@/routing/Link';
 
-export function Pagination({ }){
+export function Pagination({ current }: {current: number}){
 
     return <div className="join">
-    <Link routeKey="홈" params={{ page: 1}} className="join-item btn">1</Link>
+    <Link routeKey="홈" params={{ page: 1}} className="join-item btn" aria-current={current === 1 ? "page" : undefined}>1</Link>
     <Link routeKey="홈" params={{ page: 2}} className="join-item btn aria-current-page:btn-active"
     
-    aria-current="page">2</Link>
-    <Link routeKey="홈" params={{ page: 3}} className="join-item btn">3</Link>
-    <Link routeKey="홈" params={{ page: 4}} className="join-item btn">4</Link>
+    aria-current={current === 2 ? "page" : undefined}>2</Link>
+    <Link routeKey="홈" params={{ page: 3}} className="join-item btn" aria-current={current === 3 ? "page" : undefined}>3</Link>
+    <Link routeKey="홈" params={{ page: 4}} className="join-item btn" aria-current={current === 4 ? "page" : undefined}>4</Link>
   </div>
 }
