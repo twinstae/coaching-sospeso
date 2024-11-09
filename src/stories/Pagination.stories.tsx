c336dc0a6aa9774fe0e0d4c5a8ded5848b906942
Taehee Kim <rabolution@gmail.com>
2024-11-09 12:56:00 +0900

    fix: storybook에도 arg 추가

--- src/stories/Pagination.stories.tsx
+++ src/stories/Pagination.stories.tsx
@@ -10,6 +10,27 @@ type Story = StoryObj<typeof Pagination>;
 
 export const Base: Story = {
   args: {
+    routeKey: "홈",
+    params: undefined,
     current: 2,
+    end: 4,
+  },
+};
+
+export const First: Story = {
+  args: {
+    routeKey: "홈",
+    params: undefined,
+    current: 1,
+    end: 4,
+  },
+};
+
+export const End: Story = {
+  args: {
+    routeKey: "홈",
+    params: undefined,
+    current: 4,
+    end: 4,
   },
 };
