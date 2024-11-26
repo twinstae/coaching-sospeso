8613900501e920af68b1a88c86af15cc59a1032f
Minsik Kim <kmsbernard@gmail.com>
2024-11-27 00:32:14 +0900

    API 핸들러에서 액션 대신 서비스 사용

--- src/pages/api/payment/handler.ts
+++ src/pages/api/payment/handler.ts
@@ -1,28 +1,28 @@
 import type { APIRoute } from "astro";
-import { actions } from "astro:actions";
+import { createPaymentServices, createSospesoServices } from "@/services/services";
+import { paymentRepo, sospesoRepo } from "@/actions/actions";
 
-const parseSospesoId = (linkAddParam: string) => {
+const parseSospesoId = (linkAddParam: string): string => {
   const params = JSON.parse(linkAddParam);
   return params.id;
 }
 
 // 페이플 결제 웹훅
-export const POST: APIRoute = async ({ request, callAction }) => {
+export const POST: APIRoute = async ({ request }) => {
   const params = await request.json();
   const { PCD_PAY_RST, PCD_PAY_CODE, PCD_PAY_TYPE, PCD_LINK_ADD_PARAM } = params;
- 
-  if (PCD_PAY_RST === "success" && PCD_PAY_CODE === "0000" && PCD_PAY_TYPE === "card") {
-    // 카드결제 성공
-    const paymentId = parseSospesoId(PCD_LINK_ADD_PARAM);
-    await callAction(actions.completeSospesoPayment, { sospesoId: paymentId, paymentResult: params });
-    await callAction(actions.issueSospeso, { sospesoId: paymentId });
-  }
 
-  if (PCD_PAY_RST === "success" && PCD_PAY_CODE === "BILL0000" && PCD_PAY_TYPE === "transfer") {
-    // 계좌이체 성공
+  const sospesoServices = createSospesoServices(sospesoRepo);
+  const paymentServices = createPaymentServices(paymentRepo);
+ 
+  const succeededCardPayment = PCD_PAY_RST === "success" && PCD_PAY_CODE === "0000" && PCD_PAY_TYPE === "card";
+  const succeededBankTransfer = PCD_PAY_RST === "success" && PCD_PAY_CODE === "BILL0000" && PCD_PAY_TYPE === "transfer";
+
+  if (succeededCardPayment || succeededBankTransfer) {
     const paymentId = parseSospesoId(PCD_LINK_ADD_PARAM);
-    await callAction(actions.completeSospesoPayment, { sospesoId: paymentId, paymentResult: params });
-    await callAction(actions.issueSospeso, { sospesoId: paymentId });
+    await paymentServices.completePayment(paymentId, params);
+    const payment = await paymentRepo.retrievePayment(paymentId);
+    await sospesoServices.issueSospeso(payment.command);
   }
 
   return new Response(
