1b9620e5e4d4015f7cd2acb45885a9f851a5d484
confidential-nt <youyoy689@gmail.com>
2025-01-19 03:10:57 +0900

    style: 빨간색 배경 삭제

--- src/components/SospesoTimeline.tsx
+++ src/components/SospesoTimeline.tsx
@@ -3,45 +3,42 @@ import { Coffee } from "@/shared/icons/Coffee";
 import { Letter } from "@/shared/icons/Letter";
 
 export function SospesoTimeline() {
-  // #FFD362
   return (
-    <div className="bg-red-300">
-      <ul className="timeline">
-        <li>
-          <div className="timeline-middle">
-            <div className="w-[43px] h-[43px] bg-white rounded-full flex justify-center items-center">
-              <Bean color="#FFD362" />
-            </div>
+    <ul className="timeline">
+      <li>
+        <div className="timeline-middle">
+          <div className="w-[43px] h-[43px] bg-white rounded-full flex justify-center items-center">
+            <Bean color="#FFD362" />
           </div>
-          <div className="timeline-end timeline-box bg-transparent border-transparent shadow-none">
-            발행
+        </div>
+        <div className="timeline-end timeline-box bg-transparent border-transparent shadow-none">
+          발행
+        </div>
+        <hr className="bg-white" />
+      </li>
+      <li>
+        <hr className="bg-white" />
+        <div className="timeline-middle">
+          <div className="w-[43px] h-[43px] bg-white rounded-full flex justify-center items-center">
+            <Coffee color="#FFD362" />
           </div>
-          <hr className="bg-white" />
-        </li>
-        <li>
-          <hr className="bg-white" />
-          <div className="timeline-middle">
-            <div className="w-[43px] h-[43px] bg-white rounded-full flex justify-center items-center">
-              <Coffee color="#FFD362" />
-            </div>
+        </div>
+        <div className="timeline-end timeline-box bg-transparent border-transparent shadow-none">
+          신청
+        </div>
+        <hr className="bg-white" />
+      </li>
+      <li>
+        <hr className="bg-white" />
+        <div className="timeline-middle">
+          <div className="w-[43px] h-[43px] bg-white rounded-full flex justify-center items-center">
+            <Letter color="#FFD362" />
           </div>
-          <div className="timeline-end timeline-box bg-transparent border-transparent shadow-none">
-            신청
-          </div>
-          <hr className="bg-white" />
-        </li>
-        <li>
-          <hr className="bg-white" />
-          <div className="timeline-middle">
-            <div className="w-[43px] h-[43px] bg-white rounded-full flex justify-center items-center">
-              <Letter color="#FFD362" />
-            </div>
-          </div>
-          <div className="timeline-end timeline-box bg-transparent border-transparent shadow-none">
-            사용
-          </div>
-        </li>
-      </ul>
-    </div>
+        </div>
+        <div className="timeline-end timeline-box bg-transparent border-transparent shadow-none">
+          사용
+        </div>
+      </li>
+    </ul>
   );
 }
