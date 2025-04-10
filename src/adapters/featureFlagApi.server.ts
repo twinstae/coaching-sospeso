0a21d899397876ba9212402a3f246ca570d0b559
Taehee Kim <rabolution@gmail.com>
2025-04-10 22:36:30 +0900

    fix: 일단 unleash 제거

--- src/adapters/featureFlagApi.server.ts
+++ src/adapters/featureFlagApi.server.ts
@@ -1,12 +1,12 @@
-import { startUnleash } from "unleash-client";
-import { secretEnv } from "./env.secret";
+// import { startUnleash } from "unleash-client";
+// import { secretEnv } from "./env.secret";
 import { isProd } from "./env.public";
 
-const unleashServerPromise = startUnleash({
-  url: "https://unleash.life-lifter.com/api",
-  appName: "coaching-sospeso-server",
-  customHeaders: { Authorization: secretEnv.SECRET_UNLEASH_TOKEN },
-});
+// const unleashServerPromise = startUnleash({
+//   url: "https://unleash.life-lifter.com/api",
+//   appName: "coaching-sospeso-server",
+//   customHeaders: { Authorization: secretEnv.SECRET_UNLEASH_TOKEN },
+// });
 
 type FlagKeys = "isFlagOn";
 
@@ -14,15 +14,15 @@ export type FeatureFlagServerApiI = {
   getIsEnabled(key: FlagKeys): Promise<boolean>;
 };
 
-const unleashFeatureFlagServerApi = {
-  getIsEnabled: async (key) => {
-    return unleashServerPromise
-      .then((unleashServer) => {
-        return unleashServer.isEnabled(key);
-      })
-      .catch(() => false);
-  },
-} satisfies FeatureFlagServerApiI;
+// const unleashFeatureFlagServerApi = {
+//   getIsEnabled: async (key) => {
+//     return unleashServerPromise
+//       .then((unleashServer) => {
+//         return unleashServer.isEnabled(key);
+//       })
+//       .catch(() => false);
+//   },
+// } satisfies FeatureFlagServerApiI;
 
 const fakeFeatureFlagServerApi = {
   getIsEnabled: async (_key) => {
@@ -30,6 +30,4 @@ const fakeFeatureFlagServerApi = {
   },
 } satisfies FeatureFlagServerApiI;
 
-export const featureFlagServerApi: FeatureFlagServerApiI = isProd
-  ? unleashFeatureFlagServerApi
-  : fakeFeatureFlagServerApi;
+export const featureFlagServerApi: FeatureFlagServerApiI = fakeFeatureFlagServerApi;
